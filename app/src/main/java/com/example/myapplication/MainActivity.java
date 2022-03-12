c2647583aa84654047113cfd8162ce3ea0a7af63
tinesime <60291334+tinesime@users.noreply.github.com>
2022-03-12 15:24:39 +0100

    init 2.1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,15 +23,16 @@ public class MainActivity extends AppCompatActivity {
     protected PrintWriter output;
     protected BufferedReader input;
 
+    TextView textView = (TextView) findViewById(R.id.textView);
+    Button button = (Button) findViewById(R.id.button);
+    Button berechnen = (Button) findViewById(R.id.button2);
+    EditText textMatrikelnummer = (EditText) findViewById(R.id.textFieldMatrikelnummer);
+    EditText textOutput = (EditText) findViewById(R.id.textFieldOutput);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView textView = (TextView) findViewById(R.id.textView);
-        Button button = (Button) findViewById(R.id.button);
-        EditText textMatrikelnummer = (EditText) findViewById(R.id.textFieldMatrikelnummer);
-        EditText textOutput = (EditText) findViewById(R.id.textFieldOutput);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,13 +44,10 @@ public class MainActivity extends AppCompatActivity {
                             Socket socket = new Socket(server, port);
                             input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                             output = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-
                             String text = textMatrikelnummer.getText().toString();
                             output.println(text);
-
                             String inputText = input.readLine();
                             textOutput.setText(inputText);
-
                             input.close();
                             output.close();
                             socket.close();
@@ -61,5 +60,59 @@ public class MainActivity extends AppCompatActivity {
                 thread.start();
             }
         });
+
+        berechnen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Runnable runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Socket socket = new Socket(server, port);
+                            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                            output = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+                            String text = textMatrikelnummer.getText().toString().trim();
+                            int temp = Integer.parseInt(text);
+                            int result = temp % 7;
+                            System.out.println(result);
+                            Log.d("Result", String.valueOf(result));
+                            if (alternierendeQuersumme(text).equals("Ihr Wert ist gerade")) {
+                                output.println("Ihr Wert ist gerade");
+                            } else {
+                                output.println("Ihr Wert ist ungerade");
+                            }
+                            String inputText = input.readLine();
+                            textOutput.setText(inputText);
+                            input.close();
+                            output.close();
+                            socket.close();
+                        } catch (Exception exception) {
+                            exception.getStackTrace();
+                        }
+                    }
+                };
+                Thread thread = new Thread(runnable);
+                thread.start();
+            }
+        });
+    }
+
+    protected String alternierendeQuersumme(String text) {
+        text = textMatrikelnummer.getText().toString().trim();
+        int number = Integer.parseInt(text);
+        int altSum = 0;
+        for (int i = 0; i > number; i++) {
+            if (i % 2 == 0) {
+                altSum = altSum + number % 10;
+            } else {
+                altSum = altSum - number % 10;
+            }
+            number = number / 10;
+        }
+        if (altSum % 2 == 0) {
+            return "Ihr Wert ist gerade";
+        } else {
+            return "Ihr Wert ist ungerade";
+        }
     }
 }
